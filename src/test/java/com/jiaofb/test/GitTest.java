7491dce8225ccf3c2a11c7f465942a6a340e2012
“hhy” <1352963143@qq.com>
2021-06-13 20:07:53 +0800

    第一次提交

---
+++ src/test/java/com/jiaofb/test/GitTest.java
@@ -0,0 +1,16 @@
+package com.jiaofb.test;
+
+/**
+ * @Auther: iteration.0
+ * @Date: 13/6/2021 - 06 - 13 - 7:20 PM
+ * @Description: com.jiaofb.test
+ * @version: 1.0
+ */
+public class GitTest {
+    //这是 一个main方法，是程序的入口：
+    public static void main(String[] args) {
+        System.out.println("hello  git !!!");
+        System.out.println("123");
+        System.out.println("333");
+    }
+}
