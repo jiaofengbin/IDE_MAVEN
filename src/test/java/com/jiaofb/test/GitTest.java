3b14513e02eb5ffd0cf494bf4edda59bbb39e598
“hhy” <1352963143@qq.com>
2021-06-13 20:20:56 +0800

    master-Test1

--- src/test/java/com/jiaofb/test/GitTest.java
+++ src/test/java/com/jiaofb/test/GitTest.java
@@ -7,7 +7,7 @@ package com.jiaofb.test;
  * @version: 1.0
  */
 public class GitTest {
-    //这是 一个main方法，是程序的入口：
+    //这是 一个main方法，是程序的入口:
     public static void main(String[] args) {
         System.out.println("hello  git !!!");
         System.out.println("123");
---
+++ src/test/java/com/jiaofb/test/Test1.java
@@ -0,0 +1,13 @@
+package com.jiaofb.test;
+
+/**
+ * @Auther: iteration.0
+ * @Date: 13/6/2021 - 06 - 13 - 8:19 PM
+ * @Description: com.jiaofb.test
+ * @version: 1.0
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("master Test1");
+    }
+}
