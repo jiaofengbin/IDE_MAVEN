9b6a2ad087690630471cff3466bd3baa7e0872a9
“hhy” <1352963143@qq.com>
2021-07-03 21:55:22 +0800

    777

--- src/test/java/com/jiaofb/test/HelloWorld.java
+++ src/test/java/com/jiaofb/test/HelloWorld.java
@@ -9,6 +9,6 @@ package com.jiaofb.test;
 public class HelloWorld {
     //这是 一个main方法，是程序的入口：
     public static void main(String[] args) {
-        System.out.println("Hello 123World!");
+        System.out.println("HelloWorld!");
     }
 }
